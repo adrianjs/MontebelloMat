9a9175aefc386428498b9b79809799550764a7d6
Henrik Høybakk Olsvik <henrik-olsvik@outlook.com>
2017-09-15 17:18:44 +0200

    updated nodetest.js making no more changes on server
    .

--- nodetest.js
+++ nodetest.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+
+
+app.use(express.static(__dirname));
+app.use('/', express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.sendfile('index.html', {root: __dirname}); 
+  res.sendFile('/pages/home.html', {root: __dirname});
+})
+
+app.get('/t', function (req, res){
+        res.send(__dirname + '/img')
 })
 
 app.listen(8082, function () {
