eaaec31e640c45964ff01cc609f4cd377dfc045a
Henrik Høybakk Olsvik <henro@stud.ntnu.no>
2017-09-15 15:28:00 +0200

    sending videre til index fra reply
    ...

--- js/nodetest.js
+++ js/nodetest.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('../index.html')
 })
 
 app.listen(8082, function () {
