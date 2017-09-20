8722699c6b7e3a568719f99e6a2995032fb7a9f6
Henrik Høybakk Olsvik <henrik-olsvik@outlook.com>
2017-09-20 18:51:48 +0200

    fungerende server.js i levende path
    håper det fungerer

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname+'/pages')));
+app.use('/', express.static(__dirname));
+
+app.get('/', function (req, res){
+	res.sendFile('pages/home.html', {root: __dirname})
+});
+
+app.listen(8082, function () {
+  console.log(__dirname)
+  console.log('Montebello foods webserver listening on port 8082!')
+}
