75ecfde9d3649a0db29a81e4ffa2a51f3f9ca064
RovelMan <jotdeepgahla@hotmail.com>
2017-09-21 17:22:37 +0200

    working slideshow

--- js/script.js
+++ js/script.js
@@ -11,17 +11,19 @@ $(window).scroll(function(e) {
     parallax();
 });
 
-/**AUTOMATIC SLIDESHOW**/
-var slideIndex = 0;
-carousel();
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
+$(document).ready(function(){
+    /**AUTOMATIC SLIDESHOW**/
+    var slideIndex = 0;
+    carousel();
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none"; 
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1} 
+            x[slideIndex-1].style.display = "flex"; 
+        setTimeout(carousel, 2000); // Change image every 2 seconds
     }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-        x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+});
