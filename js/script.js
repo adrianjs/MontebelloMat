45e8e56f90f9aff11b11b2c13a7fc3e589b50aa4
RovelMan <jotdeepgahla@hotmail.com>
2017-09-21 17:01:52 +0200

    starters picture ratio fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 //Parallax effect (obviously)
 function parallax() {
     var scrolled = $(window).scrollTop();
-    console.log("hello");
-    console.log(scrolled);
-    console.log(-(scrolled * 1.2));
+    //console.log("hello");
+    //console.log(scrolled);
+    //console.log(-(scrolled * 1.2));
     $('#monte').css('top', -(scrolled * 1.2) + 'px');
 }
 
 $(window).scroll(function(e) {
     parallax();
 });
+
+/**AUTOMATIC SLIDESHOW**/
+var slideIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+        x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
