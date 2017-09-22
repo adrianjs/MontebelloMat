81ea77a817721f79699539d11c70665a15cec273
exenpipp <baldur.kjelsvik@gmail.com>
2017-09-22 11:16:11 +0200

    Endringer i CSS og js navbar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 //Parallax effect (obviously)
 function parallax() {
     var scrolled = $(window).scrollTop();
-    console.log("hello");
-    console.log(scrolled);
-    console.log(-(scrolled * 1.2));
     $('#monte').css('top', -(scrolled * 1.2) + 'px');
 }
 
 $(window).scroll(function(e) {
     parallax();
 });
+
+
+//Show and hide navbar
+$(document).ready(function(){
+    $("#navpic").click(function(){
+        $("#flex-nav").toggle();
+    });
+});
+
+
+let width = $(window).width(); 
+
+
+$(window).resize(function() {
+    width = $(window).width();
+    console.log("sjekker");
+    checkWidth()
+});
+
+
+function checkWidth(){
+	if (width >= 650) {
+		$("#flex-nav").css('display','flex');
+	}
+	else {
+		$("#flex-nav").css('display','none');
+	}
+}
