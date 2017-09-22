95d586a72f51ac78fc7892f923c381002eb13c41
RovelMan <jotdeepgahla@hotmail.com>
2017-09-22 13:25:23 +0200

    removed unused code

--- js/script.js
+++ js/script.js
@@ -37,20 +37,3 @@ function checkWidth(){
 		$("#flex-nav").css('display','none');
 	}
 }
-
-$(document).ready(function(){
-    /**AUTOMATIC SLIDESHOW**/
-    var slideIndex = 0;
-    //carousel();
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none"; 
-        }
-        slideIndex++;
-        if (slideIndex > x.length) {slideIndex = 1} 
-            x[slideIndex-1].style.display = "flex"; 
-        setTimeout(carousel, 6000); // Change image every 2 seconds
-    }
-});
