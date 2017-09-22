3c38d5874815978c3c6b890a1c23b115706bd597
Henrik Høybakk Olsvik <henrik-olsvik@outlook.com>
2017-09-22 14:01:34 +0200

    publishing to merge
    desy

--- js/ajaxGuestsScript.js
+++ js/ajaxGuestsScript.js
@@ -12,8 +12,9 @@ $(document).ready(function(){
 });
 
 function ajaxHandler(){
-    console.log(items);
-    console.log(items[quoteCounter]);
+    if(quoteCounter > 6){
+      quoteCounter = 0;
+    }
     $("#comment-text").html(items[quoteCounter]); 
     $("#costumer-pic").attr("src",(items[quoteCounter+1]));
     quoteCounter = quoteCounter + 2;
