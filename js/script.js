6ed6364014b43ac291347038a2fec9a93e018a06
RovelMan <jotdeepgahla@hotmail.com>
2017-09-23 15:00:55 +0200

    added parallax

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-//Parallax effect (obviously)
-function parallax() {
-    var scrolled = $(window).scrollTop();
-    //console.log("hello");
-    //console.log(scrolled);
-    //console.log(-(scrolled * 1.2));
-    $('#monte').css('top', -(scrolled * 1.2) + 'px');
-}
-
-$(window).scroll(function(e) {
-    parallax();
-});
-
 //Show and hide navbar
 $(document).ready(function(){
     $("#navpic").click(function(){
