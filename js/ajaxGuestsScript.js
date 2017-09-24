a5bd210d1779a9041f782c47de26134f2eba2df8
Henrik Høybakk Olsvik <henrik-olsvik@outlook.com>
2017-09-24 17:12:31 +0200

    Removed unused quickload and returned to json
    ...

--- js/ajaxGuestsScript.js
+++ js/ajaxGuestsScript.js
@@ -2,12 +2,20 @@ var quoteCounter = 0;
 var lines;
 
 $(document).ready(function(){
-    $.ajax({url: "../js/quotes.txt", success: function(result){
-      lines = result.split("\n");
+  $.ajax({ 
+    type: 'GET', 
+    url: '../js/quotes.json', 
+    data: { get_param: 'value' }, 
+    dataType: 'json',
+    success: function (data) { 
+    $.each( data, function( key, val ) { 
+      lines.push(val);
       ajaxHandler();
       setInterval(ajaxHandler, 15000);
-    }});
+    });
+  }});
 });
+
 
 function ajaxHandler(){
   if(quoteCounter > 6){
